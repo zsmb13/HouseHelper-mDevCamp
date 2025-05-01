88c9abdd9716e437d01e4f87b3ec7c6a6a3674c2
Márton Braun <marton.braun@jetbrains.com>
2025-05-01 15:53:57 +0200

    Task 7: Using rememberSaveable

--- shared/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/dashboard/RoomsContent.kt
+++ shared/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/dashboard/RoomsContent.kt
@@ -36,6 +36,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -85,7 +86,7 @@ fun RoomsContent(
             }
             val devices by roomViewModel.devices.collectAsStateWithLifecycle()
 
-            var expanded by remember { mutableStateOf(true) }
+            var expanded by rememberSaveable { mutableStateOf(true) }
 
             RoomSection(
                 room = room,
