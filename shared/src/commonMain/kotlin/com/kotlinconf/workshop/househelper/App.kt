kropp/HouseHelper

Task 4: Add subtitle to onboarding

composeApp/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/App.kt
@@ -26,9 +26,12 @@ import com.kotlinconf.workshop.househelper.navigation.OnboardingDone
 import com.kotlinconf.workshop.househelper.navigation.OnboardingWelcome
 import househelper.composeapp.generated.resources.Res
 import househelper.composeapp.generated.resources.onboarding_about
+import househelper.composeapp.generated.resources.onboarding_about_subtitle
 import househelper.composeapp.generated.resources.onboarding_done
+import househelper.composeapp.generated.resources.onboarding_done_subtitle
 import househelper.composeapp.generated.resources.onboarding_next_button
 import househelper.composeapp.generated.resources.onboarding_welcome
+import househelper.composeapp.generated.resources.onboarding_welcome_subtitle
 import kotlinx.coroutines.channels.Channel
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -54,6 +57,7 @@ fun App() {
                 composable<OnboardingWelcome> {
                     OnboardingScreen(
                         text = stringResource(Res.string.onboarding_welcome),
+                        subtitle = stringResource(Res.string.onboarding_welcome_subtitle),
                         buttonText = stringResource(Res.string.onboarding_next_button),
                         icon = Icons.Default.Favorite,
                         onNext = { navController.navigate(OnboardingAbout) }
@@ -62,6 +66,7 @@ fun App() {
                 composable<OnboardingAbout> {
                     OnboardingScreen(
                         text = stringResource(Res.string.onboarding_about),
+                        subtitle = stringResource(Res.string.onboarding_about_subtitle),
                         buttonText = stringResource(Res.string.onboarding_next_button),
                         icon = Icons.Default.Info,
                         onNext = { navController.navigate(OnboardingDone) }
@@ -70,6 +75,7 @@ fun App() {
                 composable<OnboardingDone> {
                     OnboardingScreen(
                         text = stringResource(Res.string.onboarding_done),
+                        subtitle = stringResource(Res.string.onboarding_done_subtitle),
                         buttonText = stringResource(Res.string.onboarding_next_button),
                         icon = Icons.Default.Home,
                         onNext = {

zsmb13/HouseHelper-mDevCamp

shared/src/commonMain/kotlin/com/kotlinconf/workshop/househelper/App.kt
@@ -31,9 +31,12 @@ import com.kotlinconf.workshop.househelper.navigation.Screen
 import dev.zacsweers.metrox.viewmodel.LocalMetroViewModelFactory
 import househelper.shared.generated.resources.Res
 import househelper.shared.generated.resources.onboarding_about
+import househelper.shared.generated.resources.onboarding_about_subtitle
 import househelper.shared.generated.resources.onboarding_done
+import househelper.shared.generated.resources.onboarding_done_subtitle
 import househelper.shared.generated.resources.onboarding_next_button
 import househelper.shared.generated.resources.onboarding_welcome
+import househelper.shared.generated.resources.onboarding_welcome_subtitle
 import kotlinx.coroutines.channels.Channel
 import org.jetbrains.compose.resources.stringResource
 
@@ -70,6 +73,7 @@ fun App(appGraph: AppGraph) {
                         entry<OnboardingWelcome> {
                             OnboardingScreen(
                                 text = stringResource(Res.string.onboarding_welcome),
+                                subtitle = stringResource(Res.string.onboarding_welcome_subtitle),
                                 buttonText = stringResource(Res.string.onboarding_next_button),
                                 icon = Icons.Default.Favorite,
                                 onNext = { backStack.add(OnboardingAbout) }
@@ -78,6 +82,7 @@ fun App(appGraph: AppGraph) {
                         entry<OnboardingAbout> {
                             OnboardingScreen(
                                 text = stringResource(Res.string.onboarding_about),
+                                subtitle = stringResource(Res.string.onboarding_about_subtitle),
                                 buttonText = stringResource(Res.string.onboarding_next_button),
                                 icon = Icons.Default.Info,
                                 onNext = { backStack.add(OnboardingDone) }
@@ -86,6 +91,7 @@ fun App(appGraph: AppGraph) {
                         entry<OnboardingDone> {
                             OnboardingScreen(
                                 text = stringResource(Res.string.onboarding_done),
+                                subtitle = stringResource(Res.string.onboarding_done_subtitle),
                                 buttonText = stringResource(Res.string.onboarding_next_button),
                                 icon = Icons.Default.Home,
                                 onNext = {